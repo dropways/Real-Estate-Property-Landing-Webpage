10d99e7b4c9dd81777e081e40b14d450fa44c572
Ankit Hingarajiya <ankit@krishaweb.net>
2022-01-07 11:07:44 +0530

    create mobile responsive layout

--- vendors/js/script.js
+++ vendors/js/script.js
@@ -10895,36 +10895,8 @@ jQuery(window).on("scroll load", function () {
 	}
 });
 jQuery(document).ready(function () {
-	/*==============================================================*/
-	// click to scroll section start
-	/*==============================================================*/
-	jQuery(".goto-section").on("click", function (e) {
-		e.preventDefault();
-		var target = jQuery(this).data("id");
-		jQuery("html, body")
-			.stop()
-			.animate(
-				{
-					scrollTop: jQuery("#" + target).offset().top,
-				},
-				1600,
-				"swing",
-				function () {}
-			);
-		if (jQuery(window).width() < 768) {
-			jQuery("html, body")
-				.stop()
-				.animate(
-					{
-						scrollTop: jQuery("#" + target).offset().top - 70,
-					},
-					1600,
-					"swing",
-					function () {}
-				);
-		}
+	jQuery(".menu-toggle").on("click", function () {
+		jQuery(this).toggleClass("open");
+		jQuery(".header-right").toggleClass("show");
 	});
-	/*==============================================================*/
-	// click to scroll section end
-	/*==============================================================*/
 });
